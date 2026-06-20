7ca5c097b5d53b25ed1c36ddd4fd0ec3e52d5324
Agata Skorupka <agata.skorupka@gmail.com>
2026-06-20 15:10:19 +0200

    feat: adjust code to lambda

--- 01-ci-lab/app/calculator.py
+++ 01-ci-lab/app/calculator.py
@@ -4,3 +4,34 @@ def add(a, b):
 
 def divide(a, b):
     return a / b
+
+
+def lambda_handler(event, context):
+    # Function URL / API Gateway
+    if "body" in event:
+        if isinstance(event["body"], str):
+            body = json.loads(event["body"])
+        else:
+            body = event["body"]
+    # Test from AWS Console
+    else:
+        body = event
+
+    a = body["a"]
+    b = body["b"]
+    operation = body["operation"]
+
+    if operation == "add":
+        result = add(a, b)
+    elif operation == "divide":
+        result = divide(a, b)
+    else:
+        return {
+            "statusCode": 400,
+            "body": json.dumps({"error": "Unsupported operation"})
+        }
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps({"result": result})
+    }
